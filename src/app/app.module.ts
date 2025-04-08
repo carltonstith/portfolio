ba3e57f5c0638c2825ae97a1b00e71b6482907c8
Carlton Stith <stith@temple.edu>
2025-04-08 14:55:15 -0400

    Reapply "Merge pull request #12 from carltonstith/server-side-rendering"
    This reverts commit 4589b976f87a0ac157733a6c0758357d3b2b6118.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
@@ -37,5 +37,5 @@ import { BlogComponent } from './components/blog/blog.component';
     bootstrap: [AppComponent], imports: [BrowserModule,
         NgbModule,
         RouterModule,
-        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi()), provideClientHydration(withEventReplay())] })
 export class AppModule { }
